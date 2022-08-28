dc751bf771058f9f552a4375e86391d909d69cf6
DESKTOP-TU68I8R\hai <phuongnguyen8910@gmail.com>
2022-08-28 21:09:22 +0700

    commit source code project 02 phuongntm18

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # aws-udacity-project-02
 Udacity project 02
+Elastic beanstalk endpoint: http://aws-phuongntm18-project-02-env.us-east-1.elasticbeanstalk.com/filteredimage?image_url=https://d1v72txp4rf8db.cloudfront.net/media/wysiwyg/Tombow_July2022_Mobile.jpg
---
+++ src/server.ts
@@ -0,0 +1,63 @@
+import express, { Request, Response} from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+  app.get("/filteredimage", async (req:Request, res:Response) => {
+    const imageURL = req.query.image_url;
+    if (!imageURL) {
+      return res.status(400).send("URL is required.");
+    }
+
+    filterImageFromURL(imageURL).then((response) => {
+      res.setHeader('Content-Type', 'image/jpg');
+      res.status(200).sendFile(response)
+      return response
+    }).then((response) => {
+      let arrayFilePath = [response]
+      setTimeout(() => {
+        deleteLocalFiles(arrayFilePath)
+      }, 500)
+    }).catch((e) => {
+      res.status(422).send('Filter image fails with error: ' + e);
+    })
+  })
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
